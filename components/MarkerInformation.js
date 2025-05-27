07d7b3bda6c4c9c14f813b43af51d1af66fcc0dd
oscarokker <oscarrode99@gmail.com>
2025-05-27 09:51:51 +0200

    Fixed errors and improved indentation in MarkerInformation

--- components/MarkerInformation.js
+++ components/MarkerInformation.js
@@ -19,154 +19,153 @@ import textilesIcon from "../assets/icons/WasteCategories/textiles (selected).pn
 import profileJens from "../assets/profile-picture-jens.png";
 
 const wasteIcons = {
-    plasticIcon,
-    batteriesIcon,
-    bulkyWasteIcon,
-    cardboardIcon,
-    cartonsIcon,
-    electronicsIcon,
-    foodWasteIcon,
-    gardenWasteIcon,
-    glassIcon,
-    hazardousWasteIcon,
-    indoorWoodIcon,
-    medicineIcon,
-    metalIcon,
-    paperIcon,
-    residualIcon,
-    textilesIcon,
+  plasticIcon,
+  batteriesIcon,
+  bulkyWasteIcon,
+  cardboardIcon,
+  cartonsIcon,
+  electronicsIcon,
+  foodWasteIcon,
+  gardenWasteIcon,
+  glassIcon,
+  hazardousWasteIcon,
+  indoorWoodIcon,
+  medicineIcon,
+  metalIcon,
+  paperIcon,
+  residualIcon,
+  textilesIcon,
 };
 const windowWidth = Dimensions.get('window').width;
 
 const MarkerInformation = ({ title, address, image, wasteTypes }) => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.topContent}>
-                <Image
-                    style={styles.wasteImage}
-                    resizeMode="cover"
-                    source={image}
-                />
-                <View style={styles.infoContainer}>
-                    <Text style={styles.title}>{title}</Text>
-                    <Text style={styles.address}>{address}</Text>
-                    <View style={styles.timeAgo}>
-                        <Text style={styles.timeAgoText}>Cleaned xx minute ago</Text>
-                    </View>
-                </View>
-            </View>
-            <View style={styles.bottomContent}>
-                {/*<Image style={styles.wasteType} resizeMode="cover" source={wasteIcons["plasticIcon"]} />
-                <Image style={styles.wasteType} resizeMode="cover" source={wasteIcons["cardboardIcon"]} />*/}
-                {
-                    wasteTypes.map((type, index) => {
-                        const icon = wasteIcons[`${type}Icon`];
-                        return icon ? (
-                            <Image
-                                key={index}
-                                style={styles.wasteType}
-                                resizeMode="cover"
-                                source={icon}
-                            />
-                        ) : null;
-                    })
-                }
-                <View style={styles.userInfo}>
-                    <Image style={styles.profilePicture} resizeMode="cover" source={profileJens} />
-                    <View style={styles.userNameAndLevel}>
-                        <Text style={styles.userName}>Jens Jakobsen</Text>
-                        <Text style={styles.userLevel}>Level 6</Text>
-                    </View>
-                </View>
-            </View>
-        </View >);
+  return (
+    <View style={styles.container}>
+      <View style={styles.topContent}>
+        <Image
+          style={styles.wasteImage}
+          resizeMode="cover"
+          source={image}
+        />
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.address}>{address}</Text>
+          <View style={styles.timeAgo}>
+            <Text style={styles.timeAgoText}>Cleaned 1 minute ago</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bottomContent}>
+        {wasteTypes.map((type, index) => {
+          const iconKey = `${type}Icon`;
+          const icon = wasteIcons[iconKey];
+          if (!icon) return null;
+          return (
+            <Image
+              key={index.toString()}
+              style={styles.wasteType}
+              resizeMode="cover"
+              source={icon}
+            />
+          );
+        })}
+        <View style={styles.userInfo}>
+          <Image style={styles.profilePicture} resizeMode="cover" source={profileJens} />
+          <View style={styles.userNameAndLevel}>
+            <Text style={styles.userName}>Jens Jakobsen</Text>
+            <Text style={styles.userLevel}>Level 6</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        shadowColor: "rgba(0, 0, 0, 0.1)",
-        shadowOffset: {
-            width: 0,
-            height: 4
-        },
-        width: windowWidth - 16,
-        shadowRadius: 6,
-        elevation: 6,
-        shadowOpacity: 1,
-        borderRadius: 8,
-        backgroundColor: "#fff",
-        padding: 16,
-        gap: 8,
-        justifyContent: "center",
+  container: {
+    shadowColor: "rgba(0, 0, 0, 0.1)",
+    shadowOffset: {
+      width: 0,
+      height: 4
     },
-    topContent: {
-        flexDirection: "row",
-        gap: 16,
-        alignItems: "flex-start",
-        height: 100,
-        backgroundColor: "rgba(200, 0, 0, 0)"
-    },
-    wasteImage: {
-        height: 104,
-        width: 104,
-        borderRadius: 8,
-    },
-    infoContainer: {
-        flexDirection: "column",
-        alignItems: "flex-start",
-        justifyContent: "center",
-        gap: 8,
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "700",
-        lineHeight: 20,
-        color: "#000",
-        width: "100%",
-        textAlign: "left",
-        fontFamily: "Inter-Regular",
-    },
-    address: {
-        color: "#525252",
-        fontSize: 14,
-        lineHeight: 20,
-        width: "100%",
-        fontFamily: "Inter-Regular",
-    },
-    timeAgo: {
-        backgroundColor: "rgba(0, 0, 0, 0)"
-    },
-    timeAgoText: {
-        color: "#737373",
-        fontFamily: "Inter-Regular",
-        fontSize: 12,
-        lineHeight: 16,
-        width: "100%",
-    },
-    bottomContent: {
-        flexDirection: "row",
-        gap: 8,
-    },
-    wasteType: {
-        width: 48,
-        height: 48,
-    },
-    userInfo: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 8,
-    },
-    profilePicture: {
-        width: 40,
-        height: 40,
-    },
-    userName: {
-        fontWeight: "500",
-        fontFamily: "Inter-Regular",
-    },
-    userNameAndLevel: {
-        backgroundColor: "rgba(0, 0, 0, 0)",
-    }
+    width: windowWidth - 16,
+    shadowRadius: 6,
+    elevation: 6,
+    shadowOpacity: 1,
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    padding: 16,
+    gap: 8,
+    justifyContent: "center",
+  },
+  topContent: {
+    flexDirection: "row",
+    gap: 16,
+    alignItems: "flex-start",
+    height: 100,
+    backgroundColor: "rgba(200, 0, 0, 0)"
+  },
+  wasteImage: {
+    height: 104,
+    width: 104,
+    borderRadius: 8,
+  },
+  infoContainer: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    gap: 8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    lineHeight: 20,
+    color: "#000",
+    width: "100%",
+    textAlign: "left",
+    fontFamily: "Inter-Regular",
+  },
+  address: {
+    color: "#525252",
+    fontSize: 14,
+    lineHeight: 20,
+    width: "100%",
+    fontFamily: "Inter-Regular",
+  },
+  timeAgo: {
+    backgroundColor: "rgba(0, 0, 0, 0)"
+  },
+  timeAgoText: {
+    color: "#737373",
+    fontFamily: "Inter-Regular",
+    fontSize: 12,
+    lineHeight: 16,
+    width: "100%",
+  },
+  bottomContent: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  wasteType: {
+    width: 48,
+    height: 48,
+  },
+  userInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  profilePicture: {
+    width: 40,
+    height: 40,
+  },
+  userName: {
+    fontWeight: "500",
+    fontFamily: "Inter-Regular",
+  },
+  userNameAndLevel: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+  }
 });
 
 export default MarkerInformation;
