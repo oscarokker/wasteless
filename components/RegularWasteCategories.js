9ff5ae03e9e8ffd85ce9a0c5fa21bae73d8ace41
rieesen <marhj@itu.dk>
2025-05-28 15:02:22 +0200

    Select/unselect categories

--- components/RegularWasteCategories.js
+++ components/RegularWasteCategories.js
@@ -1,88 +1,109 @@
-//MADE BY MARIE HJORTH
-import React from 'react';
-import { Image, View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+//Made by Marie Hjorth
+import React, { useState } from 'react';
+import { Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+
+//Mapping of all possible categories of regular waste 
+const wasteCategoriesIcons = {
+  'food-waste': {
+    selected: require('../assets/icons/WasteCategories/food-waste (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/food-waste (unselected).png'),
+  },
+  'plastics': {
+    selected: require('../assets/icons/WasteCategories/plastics (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/plastics (unselected).png'),
+  },
+  'cartons': {
+    selected: require('../assets/icons/WasteCategories/cartons (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/cartons (unselected).png'),
+  },
+  'paper': {
+    selected: require('../assets/icons/WasteCategories/paper (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/paper (unselected).png'),
+  },
+  'glass': {
+    selected: require('../assets/icons/WasteCategories/glass (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/glass (unselected).png'),
+  },
+  'cardboard': {
+    selected: require('../assets/icons/WasteCategories/cardboard (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/cardboard (unselected).png'),
+  },
+  'metal': {
+    selected: require('../assets/icons/WasteCategories/metal (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/metal (unselected).png'),
+  },
+  'garden-waste': {
+    selected: require('../assets/icons/WasteCategories/garden-waste (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/garden-waste (unselected).png'),
+  },
+  'medicine': {
+    selected: require('../assets/icons/WasteCategories/medicine (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/medicine (unselected).png'),
+  },
+  'indoor-wood': {
+    selected: require('../assets/icons/WasteCategories/indoor-wood (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/indoor-wood (unselected).png'),
+  },
+  'residual': {
+    selected: require('../assets/icons/WasteCategories/residual (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/residual (unselected).png'),
+  },
+  'electronics': {
+    selected: require('../assets/icons/WasteCategories/electronics (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/electronics (unselected).png'),
+  },
+  'textiles': {
+    selected: require('../assets/icons/WasteCategories/textiles (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/textiles (unselected).png'),
+  },
+  'batteries': {
+    selected: require('../assets/icons/WasteCategories/batteries (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/batteries (unselected).png'),
+  },
+  'bulky-waste': {
+    selected: require('../assets/icons/WasteCategories/bulky-waste (selected).png'),
+    unselected: require('../assets/icons/WasteCategories/bulky-waste (unselected).png'),
+  },
+};
+
+const wasteCategories = Object.keys(wasteCategoriesIcons);
 
 const RegularWasteCategories = () => {
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const toggleCategory = (category) => {
+    setSelectedCategories((prev) =>
+      prev.includes(category)
+        ? prev.filter((item) => item !== category)
+        : [...prev, category]
+    );
+  };
+
   return (
     <ScrollView 
-    style={styles.horizontalContainer}
-    horizontal={true}
-    showsHorizontalScrollIndicator={false}>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/food-waste (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/plastics (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/paper (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/glass (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/cardboard (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/metal (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/cartons (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/glass (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/garden-waste (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/medicine (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/indoor-wood (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.wasteCategory}>
-      <Image
-          source={require('../assets/icons/WasteCategories/residual (unselected).png')}
-          style={styles.icon}
-        />
-      </TouchableOpacity>
-    </ScrollView>
-  );
-};
+     style={styles.horizontalContainer}
+     horizontal={true}
+     showsHorizontalScrollIndicator={false}
+    >
+      {wasteCategories.map ((category) => {
+        const isSelected = selectedCategories.includes(category);
+        const imageSource = isSelected
+         ? wasteCategoriesIcons[category].selected
+         : wasteCategoriesIcons[category].unselected;
+        
+          return (
+            <TouchableOpacity
+              key={category}
+              style={styles.wasteCategory}
+              onPress={() => toggleCategory(category)}
+            >
+              <Image source={imageSource} style={styles.icon} />
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    );
+  };  
 
 const styles = StyleSheet.create({
   horizontalContainer: {
